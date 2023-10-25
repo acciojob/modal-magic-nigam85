6c387c7df8eda7fffcb577138a5fb35c2f099fff
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-25 21:25:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+const openModalButton = document.querySelector('.open-modal-button');
+const modalContainer = document.querySelector('.modal-container');
+const closeButton = document.querySelector('.close-button');
+
+openModalButton.addEventListener('click', () => {
+    modalContainer.style.display = 'flex';
+});
+
+closeButton.addEventListener('click', () => {
+    modalContainer.style.display = 'none';
+});
+
+window.addEventListener('click', (event) => {
+    if (event.target === modalContainer) {
+        modalContainer.style.display = 'none';
+    }
+});
